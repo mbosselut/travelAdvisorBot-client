0984aefea2b07e18ccd4fcf5c99956cf19f5bd31
mbosselut <duchmol7@hotmail.com>
2019-12-13 00:12:46 +0100

    Add all pictures

--- src/images.js
+++ src/images.js
@@ -1,5 +1,85 @@
 export const images = [
   {
+    country: "Albania",
+    pictures: [
+      "https://i2.wp.com/euroviajar.com/wp-content/uploads/2018/09/Beautiful-Ksamili-beaches.jpg?resize=720%2C454&ssl=1",
+      "https://www.godeeptravel.com/testsite/wp-content/uploads/2017/12/Berat.jpg",
+      "http://static1.squarespace.com/static/5940f2725016e1c79e469470/594123ea8748e39cae3a00aa/59c96d224c0dbf45c958f80b/1568799727731/Beaches+Albania.jpg?format=2500w"
+    ]
+  },
+  {
+    country: "Azerbaijan",
+    pictures: [
+      "https://www.onlinevisa.com/static/dd0bebfd5eb04b605cf05292fb3c83cb/bc3a8/azerbaijan-capital-baku-entry-visas.jpg",
+      "http://cdn.cnn.com/cnnnext/dam/assets/180418131822-03-azerbaijan-best-things-to-see-and-do.jpg",
+      "https://www.culturepartnership.eu/upload/news/55dca2381e2eb.jpg"
+    ]
+  },
+  {
+    country: "Bali",
+    pictures: [
+      "https://d22ir9aoo7cbf6.cloudfront.net/wp-content/uploads/sites/4/2019/05/Nusa-Lembongan-best-beaches-in-Bali.jpg",
+      "https://img.jakpost.net/c/2019/03/19/2019_03_19_67991_1552969698._large.jpg",
+      "https://www.vakantiedealz.nl/wp-content/uploads/2018/09/Bali-Kust-Klif-Zee-1000x640-1000x640.jpg"
+    ]
+  },
+  {
+    country: "Chile",
+    pictures: [
+      "https://chile.travel/wp-content/uploads/bfi_thumb/Parque-Nacional-Torres-del-Paine-tan-fascinante.-mq6z58cbqm9ailql44n38tvhzl8dcp35p6viz0xgts.png",
+      "https://www.azamara.com/sites/default/files/heros/chile-1800x1000.jpg",
+      "https://www.globotreks.com/wp-content/uploads/2015/05/Moai-Ranu-Raraku.jpg"
+    ]
+  },
+  {
+    country: "Colombia",
+    pictures: [
+      "https://www.beyondmyborder.com/wp-content/uploads/2018/11/communa-13-view.jpg",
+      "https://valledellili.org/wp-content/uploads/2018/04/Carrousel-sobrecali-2.jpg",
+      "https://www.medellincitytours.com/wp-content/uploads/2018/12/679-1080x675.jpg"
+    ]
+  },
+  {
+    country: "Croatia",
+    pictures: [
+      "https://i.pinimg.com/originals/c1/4a/b9/c14ab94235cc413471b682d67a9dc5a9.jpg",
+      "https://lp-cms-production.imgix.net/features/2017/08/GettyImages-156940828_high-2c5012d5db15.jpg?q=25",
+      "http://flavorverse.com/wp-content/uploads/2018/07/Croatia-Beaches.jpg"
+    ]
+  },
+  {
+    country: "Ecuador",
+    pictures: [
+      "https://images.squarespace-cdn.com/content/v1/53172b1be4b0cc8a5401db4a/1500944973342-LJ50ZGF68M2M9MTAAYO1/ke17ZwdGBToddI8pDm48kPqQfq0L3n3wpHIsRapTfg8UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKczo5Zn4xktlpMsMj-QlHXeMfNK6GwvtVkYEWiR8XAPyD3GfLCe_DXOSC_YcAacWL_/image-asset.jpeg",
+      "https://www.homesgofast.com/media/4d9857b85c36fa647be60e189222d22797ab4f0f25d42ef69cc765b2d7f43762.jpg",
+      "https://www.anywhere.com/img-b/vacations/ecuador/themes/beach/Ecuador_Beach0001.jpg"
+    ]
+  },
+  {
+    country: "Ethiopia",
+    pictures: [
+      "https://www.thenational.ae/image/policy:1.797407:1543503906/GettyImages-904630600.jpg?f=16x9&w=1200&$p$f$w=408d975",
+      "https://cdn.theatlantic.com/assets/media/img/photo/2017/06/beauty-and-color-scenes-from-ethiop/e01_133860147-1/original.jpg",
+      "https://i.pinimg.com/originals/3f/6f/4f/3f6f4f3fba0597ef99416ef0bb2d72c9.jpg"
+    ]
+  },
+  {
+    country: "Egypt",
+    pictures: [
+      "https://cdn.egypttoursportal.com/uploads/2017/11/Cairo-City-Egypt-Tours-Portal-1-e1511901150793.jpg",
+      "https://cdn-img.instyle.com/sites/default/files/styles/1600xflex/public/images/2016/11/110416-egypt-travel-guide-lead.jpg?itok=XV3p9DV3",
+      "https://images.unsplash.com/photo-1539768942893-daf53e448371?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+    ]
+  },
+  {
+    country: "Italy",
+    pictures: [
+      "https://cdn-image.travelandleisure.com/sites/default/files/styles/tnl_redesign_article_landing_page/public/videos/florence_0.jpg?itok=j0iKOuOp",
+      "https://www.roadaffair.com/wp-content/uploads/2017/09/colosseum-rome-italy-shutterstock_433413835-1024x683.jpg",
+      "https://www.traveller.com.au/content/dam/images/g/z/9/i/u/n/image.related.articleLeadwide.620x349.gz9ir0.png/1509586048094.jpg"
+    ]
+  },
+  {
     country: "Japan",
     pictures: [
       "https://www.realgap.co.uk/tpl/lib/img/public/compressed-images/tpl/lib/img/private/media/1721_496829791__website__-_crop-1200x0-85.jpg",
@@ -8,11 +88,107 @@ export const images = [
     ]
   },
   {
-    country: "Ethiopia",
+    country: "Kenya",
     pictures: [
-      "https://www.realgap.co.uk/tpl/lib/img/public/compressed-images/tpl/lib/img/private/media/1721_496829791__website__-_crop-1200x0-85.jpg",
-      "https://d1slf0jqvy1d13.cloudfront.net/www.joker.be/production/field_collection/header/image/miyajimaitsukushimanicki-eliza-schinow-plfde8.jpg?auto=format&fit=max&fm=jpg&h=940&ixlib=php-1.1.0&q=60&w=2560&s=e6e17e97d87ae5f94d36d3793f2eca93",
-      "https://www.telegraph.co.uk/content/dam/Travel/2019/August/iStock-1047662500.jpg?imwidth=450"
+      "https://traveldiscoverkenya.com/wp-content/uploads/2018/03/a.jpg",
+      "http://all4desktop.com/data_images/original/4234598-kenya.jpg",
+      "https://naturalcapitalcoalition.org/wp-content/uploads/2018/05/Mangrove_swamp_Iriomote_Island_Okinawa_Japan.jpg"
+    ]
+  },
+  {
+    country: "Kyrgyzstan",
+    pictures: [
+      "https://lp-cms-production.imgix.net/2019-06/193389644ea17718c4e3ccf969e7f1e5-kyrgyzstan.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4",
+      "https://www.wildernesstravel.com/images/trips/asia/kyrgyzstan/kyrgyzstan-hiking-in-the-celestial-mountains/1-slide-kyrgysztan-yurts-at-tash-raba-med.jpg",
+      "https://www.nationalgeographic.com/content/dam/travel/2019-digital/wild-beautiful-kyrgyzstan/kyrgyzstan-kol-suu.jpg"
+    ]
+  },
+  {
+    country: "Lanka",
+    pictures: [
+      "https://www.letstravel.lk/wp-content/uploads/2019/02/Yala_1500x630.jpg",
+      "https://cdn.travelwireasia.com/wp-content/uploads/2018/12/shutterstock_644007904-897x500.jpg",
+      "https://cdn.travelwireasia.com/wp-content/uploads/2016/02/Sri-Lanka-eco-tourism-e1469776992926-897x500.jpg"
+    ]
+  },
+  {
+    country: "Lithuania",
+    pictures: [
+      "https://cdn-04.independent.ie/life/travel/europe/article38054146.ece/44f7a/AUTOCROP/w620/2019-04-28_lif_49818362_I1.JPG",
+      "https://sumfinity.com/wp-content/uploads/2018/05/Vilnius-Skyline-Lithuania-winter-evening.jpg",
+      "https://www.worldtravelguide.net/wp-content/uploads/2017/03/shu-Lithuania-Trakai-762676237-1440x823.jpg"
+    ]
+  },
+  {
+    country: "Madagascar",
+    pictures: [
+      "https://www.thoughtco.com/thmb/8Prhf83RDQS-IRy0FoN4dNS5V0E=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/madagascar-682b99a278eb4f2dbc7cf3b8f4f18068.jpg",
+      "https://nosybe-tourisme.com/wp-content/uploads/2017/04/Nosy-tanikely-2-1024x683.jpg",
+      "https://previews.123rf.com/images/pierivb/pierivb1710/pierivb171000340/89000477-the-great-tsingy-de-bemaraha-of-madagascar-in-the-tsingy-de-bemaraha-integral-nature-reserve-of-unes.jpg"
+    ]
+  },
+  {
+    country: "Mexico",
+    pictures: [
+      "https://imagenescityexpress.scdn6.secure.raxcdn.com/sites/default/files/2018-05/portada-5-playas-en-mexico.jpg",
+      "https://www.thesun.co.uk/wp-content/uploads/2017/02/nintchdbpict000295572415-e1486036234177.jpg",
+      "https://www.sundaypost.com/wp-content/uploads/sites/13/2019/02/iStock-468386554.jpg"
+    ]
+  },
+  {
+    country: "Montenegro",
+    pictures: [
+      "https://img.theculturetrip.com/768x432/wp-content/uploads/2018/01/biograd_lake.jpg",
+      "https://lp-cms-production.imgix.net/features/2014/12/Rafting-Tara-canyon-Montenegro-NTO.jpg?w=1200&sharp=10&vib=20",
+      "https://cdn.pixabay.com/photo/2019/07/16/06/28/montenegro-4341150_960_720.jpg"
+    ]
+  },
+  {
+    country: "Mozambique",
+    pictures: [
+      "https://travelafricamag.com/wp-content/uploads/2016/06/Nkwichi-Lodge-Lake-Niassa-Manda-Snorkelling-Frash-Water.jpg",
+      "http://www.mozambique.co.za/images/dugong-beach-lodge-boat-590x390.jpg",
+      "https://daddysdeals.co.za/sites/default/files/the_top_6_reasons_to_visit_mozambique_in_2019_0.jpg"
+    ]
+  },
+  {
+    country: "Philippines",
+    pictures: [
+      "https://lp-cms-production.imgix.net/features/2015/04/elnido-698fa718c90a.jpg?q=25",
+      "https://www.touropia.com/gfx/d/best-beaches-in-the-philippines/marimegmeg_beach_palawan.jpg?v=1",
+      "https://www.investopedia.com/thmb/ImL1yAoeYuFWFcR6ClHYkxYWKVs=/735x0/x_best_beach_towns_in_the_philippines-5bfc3429c9e77c0026b69a30.jpg"
+    ]
+  },
+  {
+    country: "Seychelles",
+    pictures: [
+      "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Africa/Seychelles/AP78907278_Seychelles_Trave.jpg?imwidth=450",
+      "https://blog.hotelscombined.com/wp-content/uploads/2019/04/Seychelles.jpg",
+      "https://lp-cms-production.imgix.net/2019-06/61921857.jpg?fit=crop&q=40&sharp=10&vib=20&auto=format&ixlib=react-8.6.4"
+    ]
+  },
+  {
+    country: "Switzerland",
+    pictures: [
+      "https://cdn.expatica.com/production/sites/9/2017/06/16182010/Lake-Oeschinen-1200x675.jpg",
+      "https://images.unsplash.com/photo-1527095655060-4026c4af2b25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      "https://trulyhandpicked.com/wp-content/uploads/2016/02/switzerland-hd-wallpaper-switzerland-nature-beauty-14553837428n4kg-1024x640.jpg"
+    ]
+  },
+  {
+    country: "Rica",
+    pictures: [
+      "https://dsvsbigncb06y.cloudfront.net/site/cruise/costa-rica/cruise-costa-rica-xxl.jpg",
+      "https://image.arrivalguides.com/415x300/12/88af71119da67f372a38ef406927ac42.jpg",
+      "https://www.simi-reizen.nl/uploads/page_images/gallery/groepsvakanties-costa-rica-1528-large.jpg"
+    ]
+  },
+  {
+    country: "USA",
+    pictures: [
+      "https://media.architecturaldigest.com/photos/5da74823d599ec0008227ea8/16:9/w_2560%2Cc_limit/GettyImages-946087016.jpg",
+      "https://cdn.vox-cdn.com/thumbor/bWyTFwGMeaHJSm_ksL7tcsWUDrg=/0x0:4529x3000/1200x675/filters:focal(1903x1138:2627x1862)/cdn.vox-cdn.com/uploads/chorus_image/image/57301301/shutterstock_110970671.0.0.jpg",
+      "https://cdn.getyourguide.com/img/tour_img-1016259-146.jpg"
     ]
   }
 ];
